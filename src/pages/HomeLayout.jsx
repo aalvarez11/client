e3c4aa41da979eef76364ef9b450ac775a7fc4fc
aalvarez11 <aalvarez1@email.arizona.edu>
2024-10-23 23:20:32 -0700

    change div to react fragment

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>navbar</nav>
+    <>
       <Outlet />
-    </div>
+    </>
   );
 };
 
